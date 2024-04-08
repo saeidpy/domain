5bae4575190045b3c412fcfa2b14f46b1f27f4eb
saeedpy <saeedpy@gmail.com>
2024-04-08 21:17:38 +0330

    feat: test create account for mint

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,13 +1,13 @@
 // place files you want to import through the `$lib` alias in this folder.
+import { web3 } from "@project-serum/anchor";
 import idl from "./solana_idl.json";
 import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
 
-/* Constants for RPC Connection the Solana Blockchain */
+export { idl }
 export const commitmentLevel = "processed";
 export const endpoint = clusterApiUrl("devnet");
 export const connection = new Connection(endpoint, commitmentLevel);
-
-/* Constants for the Deployed "Hello World" Program */
 export const programId = new PublicKey(idl.metadata.address);
 export const programInterface = JSON.parse(JSON.stringify(idl));
-export {idl}
+export const CHAINLINK_FEED = new web3.PublicKey('99B2bTijsU6f1GCT73HmdR7HCFFjGMBcPZY6jZ96ynrR')
+export const CHAINLINK_PROGRAM_ID = new web3.PublicKey('HEvSKofvBgfaexv23kMabbYqxasxU3mQ4ibBMEmJWHny')
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,16 @@
 <!-- App.svelte -->
 <script lang="ts">
-	import { connection, programId } from '$lib';
-	import { web3 } from '@project-serum/anchor';
+	import { CHAINLINK_FEED, CHAINLINK_PROGRAM_ID, connection, programId } from '$lib';
+	import { web3, Wallet } from '@project-serum/anchor';
 	import { TOKEN_PROGRAM_ID } from '@project-serum/anchor/dist/cjs/utils/token';
-	import { getAssociatedTokenAddress, getExtraAccountMetaAddress } from '@solana/spl-token';
-	import { LAMPORTS_PER_SOL } from '@solana/web3.js';
+	import {
+		getAssociatedTokenAddressSync,
+		getOrCreateAssociatedTokenAccount,
+		type Account,
+		getAccount,
+		createAssociatedTokenAccountInstruction
+	} from '@solana/spl-token';
+	import { LAMPORTS_PER_SOL, Transaction } from '@solana/web3.js';
 	import { workSpace } from '@svelte-on-solana/wallet-adapter-anchor';
 	import { walletStore } from '@svelte-on-solana/wallet-adapter-core';
 	import { WalletMultiButton } from '@svelte-on-solana/wallet-adapter-ui';
@@ -12,7 +18,7 @@
 	let initialized = false;
 	let domainName = '';
 	let metadataURI = '';
-
+	//wallet mnemonic= wash paddle silly corn glance embrace cannon armed fortune trade wrap invite
 	async function initDns() {
 		let [dnsState] = web3.PublicKey.findProgramAddressSync([Buffer.from('dns_state')], programId);
 
@@ -33,6 +39,7 @@
 				const txId = await connection.sendRawTransaction(signedTx.serialize());
 				await connection.confirmTransaction(txId);
 				console.log('🚀 ~ initDns ~ txId:', txId);
+				//initial txId: 3pjer97DJf9BJV3skKs9k7S2wj6GRaw54Cmz9Lb8Ft1sfHMJBvumXpwgFTbLfua9duxDUrgyAb11Hseyrj2jqFnT
 			} else {
 				console.log('Program already initialized');
 			}
@@ -55,19 +62,40 @@
 
 		let [dnsState] = web3.PublicKey.findProgramAddressSync([Buffer.from('dns_state')], programId);
 		let [derivedPublicKey, nonce] = web3.PublicKey.findProgramAddressSync(
-			[Buffer.from('domain'), Buffer.from('domain1')],
+			[Buffer.from('domain'), Buffer.from('domain1.solana')],
 			programId
 		);
 
-		let associatedTokenAccount = await getAssociatedTokenAddress(
-			mint.publicKey,
-			$walletStore.publicKey
-		);
+		const associatedToken = getAssociatedTokenAddressSync(mint.publicKey, $walletStore.publicKey);
+		let account: Account;
+
+		try {
+			account = await getAccount(connection, associatedToken);
+			console.log("🚀 ~ registerDomain ~ account:", account)
+		} catch (error) {
+			try {
+				const transaction = new Transaction().add(
+					createAssociatedTokenAccountInstruction(
+						$walletStore.publicKey,
+						associatedToken,
+						$walletStore.publicKey,
+						mint.publicKey
+					)
+				);
+				transaction!.feePayer = $walletStore.publicKey;
+				transaction!.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+				const signedTx = await $walletStore.signTransaction(transaction);
+				const txId = await connection.sendRawTransaction(signedTx.serialize());
+				console.log('🚀 ~ registerDomain ~ txId:', txId);
+				await connection.confirmTransaction(txId);
+			} catch (error) {}
+		}
+		console.log('🚀 ~ registerDomain ~ account:', account!);
 
 		try {
 			const tx = await $workSpace.program?.methods
 				.registerDomain(
-					'domain1',
+					'domain1.solana',
 					1,
 					mint.publicKey,
 					'https://arweave.net/y5e5DJsiwH0s_ayfMwYk-SnrZtVZzHLQDSTZ5dNRUHA',
@@ -78,16 +106,16 @@
 					domain: derivedPublicKey,
 					state: dnsState,
 					receiver: $walletStore.publicKey,
-					chainlinkFeed: new web3.PublicKey('CH31Xns5z3M1cTAbKW34jcxPPciazARpijcHj9rxtemt'),
-					chainlinkProgram: new web3.PublicKey('HEvSKofvBgfaexv23kMabbYqxasxU3mQ4ibBMEmJWHny'),
+					chainlinkFeed: CHAINLINK_FEED,
+					chainlinkProgram: CHAINLINK_PROGRAM_ID,
 					receiverAta: $walletStore.publicKey,
 					payerAta: $walletStore.publicKey,
 					mintAuthority: $walletStore.publicKey,
 					mint: mint.publicKey,
-					tokenAccount: associatedTokenAccount,
+					tokenAccount: account!.address,
 					tokenProgram: TOKEN_PROGRAM_ID,
 					metadata: $walletStore.publicKey,
-					tokenMetadataProgram: $walletStore.publicKey,
+					tokenMetadataProgram: TOKEN_PROGRAM_ID,
 					masterEdition: $walletStore.publicKey,
 					authority: $walletStore.publicKey,
 					rent: web3.SYSVAR_RENT_PUBKEY,
@@ -106,17 +134,14 @@
 			console.log('🚀 ~ registerDomain ~ error:', error);
 		}
 	}
-
-	// Initialize Solana program on component mount
-	onMount(() => {
-		// initDns();
-	});
 </script>
 
 <div class="container">
 	<div class="form-container">
 		<WalletMultiButton />
 		<h2>Solana Domain Registration</h2>
+		<button on:click={initDns}>init program</button>
+
 		<form on:submit|preventDefault={registerDomain}>
 			<label for="domainName">Domain Name:</label>
 			<input
@@ -174,7 +199,7 @@
 		box-sizing: border-box;
 	}
 
-	.form-container button[type='submit'] {
+	.form-container button {
 		width: 100%;
 		padding: 10px;
 		background-color: var(--custom-button-color);
@@ -184,7 +209,7 @@
 		cursor: pointer;
 	}
 
-	.form-container button[type='submit']:hover {
+	.form-container button:hover {
 		background-color: var(--custom-button-hover-color);
 	}
 </style>
