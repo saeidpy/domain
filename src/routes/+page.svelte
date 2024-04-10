ea48cb20e89a1ac32283141b2f1950a6ba23c488
saeedpy <saeedpy@gmail.com>
2024-04-10 14:42:21 +0330

    feat: add metaData address and masterEdtion address

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,86 +3,97 @@
 	import {
 		CHAINLINK_FEED,
 		CHAINLINK_PROGRAM_ID,
-		METADATA_SEED,
 		TOKEN_METADATA_PROGRAM_ID,
 		connection,
+		endpoint,
 		programId
 	} from '$lib';
-	import { getOrCreateAssociatedTokenAccount } from '$lib/utils/getOrCreateTokenAccount';
+	import {
+		findMasterEditionPda,
+		findMetadataPda,
+		mplTokenMetadata
+	} from '@metaplex-foundation/mpl-token-metadata';
+	import { publicKey } from '@metaplex-foundation/umi';
+	import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+	import { walletAdapterIdentity } from '@metaplex-foundation/umi-signer-wallet-adapters';
 	import { web3 } from '@project-serum/anchor';
-	import { TOKEN_PROGRAM_ID } from '@project-serum/anchor/dist/cjs/utils/token';
-	import { getAssociatedTokenAddressSync } from '@solana/spl-token';
-	import { Keypair, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
+	import { TOKEN_PROGRAM_ID, getAssociatedTokenAddress } from '@solana/spl-token';
 	import { workSpace } from '@svelte-on-solana/wallet-adapter-anchor';
 	import { walletStore } from '@svelte-on-solana/wallet-adapter-core';
 	import { WalletMultiButton } from '@svelte-on-solana/wallet-adapter-ui';
-	import * as bs58 from 'bs58';
 
-	let initialized = false;
+	let initialized = true;
+	let isLoading = false;
+	let initIsLoading = false;
 	let domainName = '';
 	let metadataURI = '';
+	const umi = createUmi(endpoint)
+		.use(walletAdapterIdentity($walletStore, true))
+		.use(mplTokenMetadata());
 	//wallet mnemonic= wash paddle silly corn glance embrace cannon armed fortune trade wrap invite
 	async function initDns() {
 		let [dnsState] = web3.PublicKey.findProgramAddressSync([Buffer.from('dns_state')], programId);
 
 		try {
-			if (!initialized) {
-				const top_domains = ['solana', 'meta', 'music', 'nft', 'web3', 'lpp', '404', '007'];
-				const tx = await $workSpace.program?.methods
-					.initDns(top_domains)
-					.accounts({
-						dnsState: dnsState,
-						signer: $walletStore.publicKey,
-						systemProgram: $workSpace.systemProgram?.programId
-					})
-					.transaction();
-				tx!.feePayer = $walletStore.publicKey;
-				tx!.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
-				const signedTx = await $walletStore.signTransaction(tx);
-				const txId = await connection.sendRawTransaction(signedTx.serialize());
-				await connection.confirmTransaction(txId);
-				console.log('🚀 ~ initDns ~ txId:', txId);
-				//initial txId: 3pjer97DJf9BJV3skKs9k7S2wj6GRaw54Cmz9Lb8Ft1sfHMJBvumXpwgFTbLfua9duxDUrgyAb11Hseyrj2jqFnT
-			} else {
-				console.log('Program already initialized');
-			}
+			initIsLoading = true;
+			const top_domains = ['solana', 'meta', 'music', 'nft', 'web3', 'lpp', '404', '007'];
+			const tx = await $workSpace.program?.methods
+				.initDns(top_domains)
+				.accounts({
+					dnsState: dnsState,
+					signer: $walletStore.publicKey,
+					systemProgram: $workSpace.systemProgram?.programId
+				})
+				.transaction();
+			const latestBlockHash = await connection.getLatestBlockhash();
+			tx!.feePayer = $walletStore.publicKey;
+			tx!.recentBlockhash = latestBlockHash.blockhash;
+			const signedTx = await $walletStore.signTransaction(tx);
+			const txId = await connection.sendRawTransaction(signedTx.serialize());
+			await connection.confirmTransaction({
+				signature: txId,
+				blockhash: latestBlockHash.blockhash,
+				lastValidBlockHeight: latestBlockHash.lastValidBlockHeight
+			});
+			initialized = true;
+			initIsLoading = false;
+			//initial txId: 3pjer97DJf9BJV3skKs9k7S2wj6GRaw54Cmz9Lb8Ft1sfHMJBvumXpwgFTbLfua9duxDUrgyAb11Hseyrj2jqFnT
 		} catch (error) {
-			console.log('🚀 ~ initDns ~ error:', error);
+			initialized = true;
+			initIsLoading = false;
 		}
 	}
 
 	// Function to register a domain
 	async function registerDomain() {
-		let mint = {
-			publicKey: new PublicKey(bs58.decode('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'))
-		};
+		isLoading = true;
+		// generate the mint account
+		const mint = web3.Keypair.generate();
 
-		// let airdropSignature = await connection.requestAirdrop(payer.publicKey, web3.LAMPORTS_PER_SOL);
+		// Derive the associated token address account for the mint
+		const associatedTokenAccount = await getAssociatedTokenAddress(
+			mint.publicKey,
+			$walletStore.publicKey
+		);
 
-		// await connection.confirmTransaction(airdropSignature);
+		console.log('🚀 ~ registerDomain ~ associatedTokenAccount:', associatedTokenAccount);
+		// derive the metadata account
+		const metadataAccount = findMetadataPda(umi, {
+			mint: publicKey(mint.publicKey)
+		})[0];
 
-		let lamportBalance;
-		const balance = await connection.getBalance($walletStore.publicKey);
-		lamportBalance = balance / LAMPORTS_PER_SOL;
+		console.log('🚀 ~ registerDomain ~ metadataAccount:', metadataAccount);
+		//derive the master edition pda
+		const masterEditionAccount = findMasterEditionPda(umi, {
+			mint: publicKey(mint.publicKey)
+		})[0];
+		console.log('🚀 ~ registerDomain ~ masterEditionAccount:', masterEditionAccount);
 
 		const [dnsState] = web3.PublicKey.findProgramAddressSync([Buffer.from('dns_state')], programId);
 		const [derivedPublicKey] = web3.PublicKey.findProgramAddressSync(
 			[Buffer.from('domain'), Buffer.from('domain1.solana')],
 			programId
 		);
-		const [metadataAddress] = web3.PublicKey.findProgramAddressSync(
-			[Buffer.from(METADATA_SEED), TOKEN_METADATA_PROGRAM_ID.toBuffer(), mint.publicKey.toBuffer()],
-			TOKEN_METADATA_PROGRAM_ID
-		);
-
-		// const tokenAccount = await getOrCreateAssociatedTokenAccount(
-		// 	connection,
-		// 	$walletStore,
-		// 	mint.publicKey,
-		// 	$walletStore.publicKey
-		// );
-		const tokenAccount = getAssociatedTokenAddressSync(mint.publicKey, $walletStore.publicKey);
-		console.log("🚀 ~ registerDomain ~ tokenAccount:", tokenAccount)
 
 		try {
 			const tx = await $workSpace.program?.methods
@@ -104,36 +115,45 @@
 					payerAta: $walletStore.publicKey,
 					mintAuthority: $walletStore.publicKey,
 					mint: mint.publicKey,
-					tokenAccount: tokenAccount,
+					tokenAccount: associatedTokenAccount,
 					tokenProgram: TOKEN_PROGRAM_ID,
-					metadata: metadataAddress,
+					metadata: metadataAccount,
 					tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
-					masterEdition: $walletStore.publicKey,
+					masterEdition: masterEditionAccount,
 					authority: $walletStore.publicKey,
 					rent: web3.SYSVAR_RENT_PUBKEY,
 					systemProgram: web3.SystemProgram.programId
 				})
 				.transaction();
 
+			const latestBlockHash = await connection.getLatestBlockhash();
 			tx!.feePayer = $walletStore.publicKey;
-			tx!.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+			tx!.recentBlockhash = latestBlockHash.blockhash;
 			const signedTx = await $walletStore.signTransaction(tx);
-
 			const txId = await connection.sendRawTransaction(signedTx.serialize());
-			await connection.confirmTransaction(txId);
+			await connection.confirmTransaction({
+				signature: txId,
+				blockhash: latestBlockHash.blockhash,
+				lastValidBlockHeight: latestBlockHash.lastValidBlockHeight
+			});
 			console.log('🚀 ~ registerDomain ~ signedTx:', txId);
 		} catch (error) {
+			isLoading = false;
+
 			console.log('🚀 ~ registerDomain ~ error:', error);
 		}
+		isLoading = false;
 	}
 </script>
 
 <div class="container">
 	<div class="form-container">
 		<WalletMultiButton />
+		<h2>Initial Program</h2>
+		<button on:click={initDns} disabled={initialized || initIsLoading}>
+			{initIsLoading ? 'Loading...' : initialized ? 'Initialed' : 'Init Program'}</button
+		>
 		<h2>Solana Domain Registration</h2>
-		<button on:click={initDns}>init program</button>
-
 		<form on:submit|preventDefault={registerDomain}>
 			<label for="domainName">Domain Name:</label>
 			<input
@@ -148,7 +168,9 @@
 			<label for="metadataURI">Metadata URI:</label>
 			<input type="text" id="metadataURI" bind:value={metadataURI} />
 
-			<button type="submit">Register Domain</button>
+			<button type="submit" disabled={isLoading}
+				>{isLoading ? 'Loading...' : 'Register Domain'}</button
+			>
 		</form>
 	</div>
 </div>
@@ -158,6 +180,7 @@
 		--custom-primary-color: #512da8;
 		--custom-button-color: #512da8;
 		--custom-button-hover-color: #311b92;
+		--custom-button-disabled-color: #5c5b62;
 	}
 
 	.container {
@@ -203,5 +226,9 @@
 
 	.form-container button:hover {
 		background-color: var(--custom-button-hover-color);
+	}
+	.form-container button:disabled {
+		cursor: auto;
+		background-color: var(--custom-button-disabled-color);
 	}
 </style>
